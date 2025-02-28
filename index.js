b61eaf210bfc4d975260d2ac5b6e44bb00680a7c
Md Khaled Bin Joha <joh60483@gmail.com>
2025-02-28 15:23:46 +0600

    Added User Model and bootstrap cdn, scripts for views.

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ const PORT = 8000;     // we'll replace it by environment variable while deployi
 app.set('view engine', 'ejs');
 app.set("views", path.resolve("./views"));
 
+// route.
+app.get("/", (req, res) =>{
+    res.render("home");
+})
+
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
